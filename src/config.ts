6bae2a9feadf3f4f39b1cd885d23f5140bd8faf9
Konstantinos Theologitis <ktheologitis@gmail.com>
2023-04-30 03:37:17 +0200

    fix: callback

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,7 @@ export const CLIENT_ID = "6937b6a3fc7e40d2a6b02f7981220753";
 export const SCOPE =
   "user-read-private user-read-email user-top-read user-library-read";
 export const REDIRECT_CALLBACK =
-  "http://localhost:3000/authorize/callback";
+  window.location.origin ===
+  "https://spotify-dashboard-red.vercel.app"
+    ? "https://spotify-dashboard-red.vercel.app/authorize/callback"
+    : "http://localhost:3000/authorize/callback";
